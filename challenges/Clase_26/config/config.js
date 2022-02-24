0bf8dc2fdd0988afaadb2d34b11e31662645b714
Bogdan Bedyukh <bogdan.bedyukh@sondeos.com.ar>
2022-02-24 13:50:16 -0300

    Folder challenges removed

--- challenges/Clase_26/config/config.js
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv'
-dotenv.config()
-
-export const PORT = process.env.PORT || 3000
-export const MONGO_URI = process.env.MONGO_URI || ''
-export const FACEBOOK_CLIENT_ID = process.env.FACEBOOK_CLIENT_ID || ''
-export const FACEBOOK_CLIENT_SECRET = process.env.FACEBOOK_CLIENT_SECRET || ''
--- challenges/Clase_28/config/config.js
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv'
-dotenv.config()
-
-export const MONGO_URI = process.env.MONGO_URI || ''
-export const FACEBOOK_CLIENT_ID = process.env.FACEBOOK_CLIENT_ID || ''
-export const FACEBOOK_CLIENT_SECRET = process.env.FACEBOOK_CLIENT_SECRET || ''
-export const NGROK_LINK = process.env.NGROK_LINK || ''
