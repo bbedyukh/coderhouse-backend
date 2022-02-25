548437f9c7385049b3332d7c53486ecaea430d7f
Bogdan Bedyukh <bogdan.bedyukh@sondeos.com.ar>
2022-02-25 09:54:49 -0300

    Class 36 Twilio finished

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
-import app from './server.js'
-import { PORT } from './config/config.js'
-import { getConnection } from './dao/db/connection.js'
+import twilio from 'twilio'
 
-getConnection()
-  .then(() => {
-    const server = app.listen(PORT, () => {
-      const message = `| Server listen on port ${PORT} |`
-      const link = `| - http://localhost:${PORT}    |`
-      console.log('-'.repeat(message.length))
-      console.log(message)
-      console.log(link)
-      console.log('-'.repeat(message.length))
-    })
-    server.on('error', (err) => console.error(`Error server: ${err}`))
-  })
-  .catch(err => {
-    console.error(err)
-  })
+const client = twilio('AC25278e5fb67a66333fb8beef4028e48d', '5f4a58fc70174458492a25b89bfb0c5f')
+
+client.messages.create({
+  from: 'whatsapp:+14155238886',
+  to: 'whatsapp:+5491167609138',
+  body: '¡SOY PELIGROSO, RAWR!',
+  mediaUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZe8xjSlSEI8a1SK92Jay9sPqJXumdLkVAAg&usqp=CAU'
+})
+  .then(result => console.log(result))
+  .catch(err => console.error(err))
