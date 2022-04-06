478f3fcb77e07f895b0c9cde0949b22961216d86
Bogdan Bedyukh <bogdan.bedyukh@sondeos.com.ar>
2022-04-06 11:12:04 -0300

    GraphQL class

--- src/dto/CartDTO.js
+++
@@ -1,9 +0,0 @@
-export default class CartDTO {
-  constructor (cart) {
-    this.id = cart._id
-    this.products = cart.products
-    this.user = cart.user
-    this.createdAt = cart.createdAt
-    this.updatedAt = cart.updatedAt
-  }
-}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,53 @@
-import Server from './server.js'
-const server = new Server()
+import express from 'express'
+import { graphqlHTTP } from 'express-graphql'
+import { buildSchema } from 'graphql'
 
-server.run()
+const app = express()
+
+app.listen(3000, () => console.log('Now listening...'))
+
+let clients = []
+let counter = 1
+
+const schema = buildSchema(`
+  type Client{
+    id: Int
+    name: String!
+    phone: String
+  }
+
+  type Query {
+    clients: [Client]
+    clientById(id: Int): Client
+  }
+
+  type Mutation {
+    createClient(name: String, phone: String): Client
+  }
+`)
+
+const root = {
+  clients: () => clients,
+  clientById: (data) => {
+    return clients.find(client => client.id === data.id)
+    // for (let i = 0; i < clients.length; i++) {
+    //   if (clients[i].id === clientId) return clients[i]
+    // }
+    // return null
+  },
+  createClient: ({ name, phone }) => {
+    const client = { id: counter, name, phone }
+    clients = [
+      ...clients,
+      client
+    ]
+    counter++
+    return client
+  }
+}
+
+app.use('/api/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true
+}))
